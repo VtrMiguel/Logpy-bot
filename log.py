324ad6a573f081b98f93f8048b57d3870e9ae1c9
Vitor Miguel <123606610+VtrMiguel@users.noreply.github.com>
2023-02-14 10:35:22 -0300

    Update log.py

--- log.py
+++ log.py
@@ -12,3 +12,11 @@ navegador.get("https://accounts.mlabs.io/accounts/sign_in?access_token=true")
 #selecionando o id do input para colocar a 'conta'
 time.sleep(7)
 navegador.find_element(By.XPATH, ('//*[@id="email"]')).send_keys("conta")
+
+#selecionando o id do input para colocar a 'senha'
+time.sleep(5)
+navegador.find_element(By.XPATH, ('//*[@id="password"]')).send_keys("senha123")
+
+#selecionando o id do botão para clicar
+time.sleep(5)
+navegador.find_element(By.XPATH, ('//*[@id="btnSubmit"]/p/div')).click()
