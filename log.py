f512eeaa02181f2a88c979b867da73ca787d5c6d
Vitor Miguel <123606610+VtrMiguel@users.noreply.github.com>
2023-02-14 10:30:39 -0300

    Update log.py

--- log.py
+++ log.py
@@ -2,3 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 from selenium.webdriver.common.by import By
+
+#abrindo o navegador desejado
+navegador = webdriver.Firefox()
+
+#abrindo o site desejado
+navegador.get("https://accounts.mlabs.io/accounts/sign_in?access_token=true")
